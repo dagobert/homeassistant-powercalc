fd7b21f37cd77b1a3c4b3c8e79126df47e842e54
CV <dagobert@users.noreply.github.com>
2024-10-20 00:18:56 +0200

    Local loader working with aliases
    The first version that is working with aliases. 
    Logging and unnecessary comments are still present.

--- custom_components/powercalc/power_profile/loader/local.py
+++ custom_components/powercalc/power_profile/loader/local.py
@@ -1,5 +1,7 @@
 import json
 import os
+import logging
+import re
 from typing import Any, cast
 
 from homeassistant.core import HomeAssistant
@@ -16,6 +18,8 @@ class LocalLoader(Loader):
         self._data_directory = directory
         self._hass = hass
         self._manufacturer_listing: dict[str, set[str]] = {}
+        _LOGGER = logging.getLogger(__name__)
+        _LOGGER.warning("local.py is executed: New object created")
 
     async def initialize(self) -> None:
         """Initialize the loader."""
@@ -76,20 +80,32 @@ class LocalLoader(Loader):
 
     async def load_model(self, manufacturer: str, model: str) -> tuple[dict, str] | None:
         """Load a model.json file from disk for a given manufacturer and model."""
-        base_dir = (
+        manufacturer_dir = (
             self._data_directory
             if self._is_custom_directory
             else os.path.join(
                 self._data_directory,
-                manufacturer.lower(),
-                model,
+                manufacturer.lower()
             )
         )
 
-        if not os.path.exists(base_dir):
+        if not os.path.exists(manufacturer_dir):
             return None
 
-        model_json_path = os.path.join(base_dir, "model.json")
+        search = {
+            model,
+            model.replace("#slash#", "/"),
+            model.lower(),
+            model.lower().replace("#slash#", "/"),
+            re.sub(r"^(.*)\(([^()]+)\)$", r"\2", model),
+        }
+
+        result = await self._get_directory_for_model(manufacturer, manufacturer_dir, search)
+        if not result:
+            raise LibraryLoadingError(f"No matching directory for {model} found")
+
+        model_dir = result[1]
+        model_json_path = os.path.join(model_dir, "model.json")
         if not model_json_path or not os.path.exists(model_json_path):
             raise LibraryLoadingError(f"model.json not found for {manufacturer} {model}")
 
@@ -99,15 +115,52 @@ class LocalLoader(Loader):
                 return cast(dict[str, Any], json.load(file))
 
         model_json = await self._hass.async_add_executor_job(_load_json)  # type: ignore
-        return model_json, base_dir
+        return model_json, model_dir
 
     async def find_model(self, manufacturer: str, search: set[str]) -> str | None:
         """Find a model for a given manufacturer. Also must check aliases."""
-        manufacturer_dir = os.path.join(self._data_directory, manufacturer)
+        manufacturer_dir = (
+            self._data_directory
+            if self._is_custom_directory
+            else os.path.join(
+                self._data_directory,
+                manufacturer.lower()
+            )
+        )
+
         if not os.path.exists(manufacturer_dir):
             return None
+        
+        result = await self._get_directory_for_model(manufacturer, manufacturer_dir, search)
+        if not result:
+            raise LibraryLoadingError(f"No matching directory for model search pattern")
+        return result[0]
 
-        model_dirs = await self._hass.async_add_executor_job(os.listdir, manufacturer_dir)
+    async def _get_directory_for_model(self, manufacturer: str, manufacturer_dir: str, search: set[str]) -> tuple[str, str] | None:
+        """Get the directory for a model. Can be an alias within the model.json"""
+        """BUGFIX: os.listdir also returns files!"""
+        dir_content = await self._hass.async_add_executor_job(os.walk, manufacturer_dir)
+        dir_content = await self._hass.async_add_executor_job(next, dir_content)
+        model_dirs = dir_content[1]
         search_lower = {phrase.lower() for phrase in search}
-
-        return next((model for model in model_dirs if model.lower() in search_lower), None)
+    
+        _LOGGER = logging.getLogger(__name__)
+  
+        for model_dir in model_dirs:
+            if model_dir.lower() in search_lower:
+                _LOGGER.warning("Found model as directory name: %s", model_dir)
+                return model_dir, os.path.join(manufacturer_dir, model_dir)
+    
+        """Check aliases within model.json files"""
+        
+        for model_dir in model_dirs:
+            json_data, directory = await self.load_model(manufacturer, model_dir)
+            aliases = json_data.get("aliases")
+            _LOGGER.warning("Aliases found in %s: %s", model_dir, aliases)
+            if aliases:
+                for alias in aliases:
+                    if alias.lower() in search_lower:
+                        _LOGGER.warning("Alias match %s in dir %s", alias, model_dir)
+                        return alias, os.path.join(manufacturer_dir, model_dir)
+        
+        return None
